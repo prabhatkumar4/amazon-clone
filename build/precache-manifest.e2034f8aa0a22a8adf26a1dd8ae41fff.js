968c0c9fa4ad45536da1dce6ecfd2e57b0d5c382
prabhat <prabhatshahi735@gmail.com>
2022-10-19 22:09:08 +0530

    project adding

---
+++ build/precache-manifest.e2034f8aa0a22a8adf26a1dd8ae41fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fcc76cff081989d63168cbd4c6bc13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6790dfbb57fb013d3098",
+    "url": "/static/css/main.2351fb45.chunk.css"
+  },
+  {
+    "revision": "bc942b8e2be945f77660",
+    "url": "/static/js/2.48919359.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.48919359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6790dfbb57fb013d3098",
+    "url": "/static/js/main.537e66b6.chunk.js"
+  },
+  {
+    "revision": "77773f3e5ca8c28393d0",
+    "url": "/static/js/runtime-main.e73e7332.js"
+  }
+]);
